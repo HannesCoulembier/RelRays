023154e2d25acf4cbf25e84c7da3a9b136cd3fcf
Hannes Coulembier <hannes.coulembier@gmail.com>
2023-11-26 16:47:29 +0100

    Fixed naming KeyCode macros

--- LoFox/src/LoFox/Core/KeyCodes.h
+++ LoFox/src/LoFox/Core/KeyCodes.h
@@ -214,127 +214,127 @@ namespace LoFox {
 }
 
 
-#define BG_KEY_SPACE              ::LoFox::Key::Space
-#define BG_KEY_APOSTROPHE         ::LoFox::Key::Apostrophe  /* ' */
-#define BG_KEY_COMMA              ::LoFox::Key::Comma		/* , */
-#define BG_KEY_MINUS              ::LoFox::Key::Minus		/* - */
-#define BG_KEY_PERIOD             ::LoFox::Key::Period		/* . */
-#define BG_KEY_SLASH              ::LoFox::Key::Slash		/* / */
-#define BG_KEY_0                  ::LoFox::Key::D0
-#define BG_KEY_1                  ::LoFox::Key::D1
-#define BG_KEY_2                  ::LoFox::Key::D2
-#define BG_KEY_3                  ::LoFox::Key::D3
-#define BG_KEY_4                  ::LoFox::Key::D4
-#define BG_KEY_5                  ::LoFox::Key::D5
-#define BG_KEY_6                  ::LoFox::Key::D6
-#define BG_KEY_7                  ::LoFox::Key::D7
-#define BG_KEY_8                  ::LoFox::Key::D8
-#define BG_KEY_9                  ::LoFox::Key::D9
-#define BG_KEY_SEMICOLON          ::LoFox::Key::Semicolon	/* ; */
-#define BG_KEY_EQUAL              ::LoFox::Key::Equal		/* = */
-#define BG_KEY_A                  ::LoFox::Key::A
-#define BG_KEY_B                  ::LoFox::Key::B
-#define BG_KEY_C                  ::LoFox::Key::C
-#define BG_KEY_D                  ::LoFox::Key::D
-#define BG_KEY_E                  ::LoFox::Key::E
-#define BG_KEY_F                  ::LoFox::Key::F
-#define BG_KEY_G                  ::LoFox::Key::G
-#define BG_KEY_H                  ::LoFox::Key::H
-#define BG_KEY_I                  ::LoFox::Key::I
-#define BG_KEY_J                  ::LoFox::Key::J
-#define BG_KEY_K                  ::LoFox::Key::K
-#define BG_KEY_L                  ::LoFox::Key::L
-#define BG_KEY_M                  ::LoFox::Key::M
-#define BG_KEY_N                  ::LoFox::Key::N
-#define BG_KEY_O                  ::LoFox::Key::O
-#define BG_KEY_P                  ::LoFox::Key::P
-#define BG_KEY_Q                  ::LoFox::Key::Q
-#define BG_KEY_R                  ::LoFox::Key::R
-#define BG_KEY_S                  ::LoFox::Key::S
-#define BG_KEY_T                  ::LoFox::Key::T
-#define BG_KEY_U                  ::LoFox::Key::U
-#define BG_KEY_V                  ::LoFox::Key::V
-#define BG_KEY_W                  ::LoFox::Key::W
-#define BG_KEY_X                  ::LoFox::Key::X
-#define BG_KEY_Y                  ::LoFox::Key::Y
-#define BG_KEY_Z                  ::LoFox::Key::Z
-#define BG_KEY_LEFT_BRACKET       ::LoFox::Key::LeftBracket		/* [ */
-#define BG_KEY_BACKSLASH          ::LoFox::Key::Backslash		/* \ */
-#define BG_KEY_RIGHT_BRACKET      ::LoFox::Key::RightBracket	/* ] */
-#define BG_KEY_GRAVE_ACCENT       ::LoFox::Key::GraveAccent		/* ` */
-#define BG_KEY_WORLD_1            ::LoFox::Key::World1			/* non-US #1 */
-#define BG_KEY_WORLD_2            ::LoFox::Key::World2			/* non-US #2 */
+#define LF_KEY_SPACE              ::LoFox::Key::Space
+#define LF_KEY_APOSTROPHE         ::LoFox::Key::Apostrophe  /* ' */
+#define LF_KEY_COMMA              ::LoFox::Key::Comma		/* , */
+#define LF_KEY_MINUS              ::LoFox::Key::Minus		/* - */
+#define LF_KEY_PERIOD             ::LoFox::Key::Period		/* . */
+#define LF_KEY_SLASH              ::LoFox::Key::Slash		/* / */
+#define LF_KEY_0                  ::LoFox::Key::D0
+#define LF_KEY_1                  ::LoFox::Key::D1
+#define LF_KEY_2                  ::LoFox::Key::D2
+#define LF_KEY_3                  ::LoFox::Key::D3
+#define LF_KEY_4                  ::LoFox::Key::D4
+#define LF_KEY_5                  ::LoFox::Key::D5
+#define LF_KEY_6                  ::LoFox::Key::D6
+#define LF_KEY_7                  ::LoFox::Key::D7
+#define LF_KEY_8                  ::LoFox::Key::D8
+#define LF_KEY_9                  ::LoFox::Key::D9
+#define LF_KEY_SEMICOLON          ::LoFox::Key::Semicolon	/* ; */
+#define LF_KEY_EQUAL              ::LoFox::Key::Equal		/* = */
+#define LF_KEY_A                  ::LoFox::Key::A
+#define LF_KEY_B                  ::LoFox::Key::B
+#define LF_KEY_C                  ::LoFox::Key::C
+#define LF_KEY_D                  ::LoFox::Key::D
+#define LF_KEY_E                  ::LoFox::Key::E
+#define LF_KEY_F                  ::LoFox::Key::F
+#define LF_KEY_G                  ::LoFox::Key::G
+#define LF_KEY_H                  ::LoFox::Key::H
+#define LF_KEY_I                  ::LoFox::Key::I
+#define LF_KEY_J                  ::LoFox::Key::J
+#define LF_KEY_K                  ::LoFox::Key::K
+#define LF_KEY_L                  ::LoFox::Key::L
+#define LF_KEY_M                  ::LoFox::Key::M
+#define LF_KEY_N                  ::LoFox::Key::N
+#define LF_KEY_O                  ::LoFox::Key::O
+#define LF_KEY_P                  ::LoFox::Key::P
+#define LF_KEY_Q                  ::LoFox::Key::Q
+#define LF_KEY_R                  ::LoFox::Key::R
+#define LF_KEY_S                  ::LoFox::Key::S
+#define LF_KEY_T                  ::LoFox::Key::T
+#define LF_KEY_U                  ::LoFox::Key::U
+#define LF_KEY_V                  ::LoFox::Key::V
+#define LF_KEY_W                  ::LoFox::Key::W
+#define LF_KEY_X                  ::LoFox::Key::X
+#define LF_KEY_Y                  ::LoFox::Key::Y
+#define LF_KEY_Z                  ::LoFox::Key::Z
+#define LF_KEY_LEFT_BRACKET       ::LoFox::Key::LeftBracket		/* [ */
+#define LF_KEY_BACKSLASH          ::LoFox::Key::Backslash		/* \ */
+#define LF_KEY_RIGHT_BRACKET      ::LoFox::Key::RightBracket	/* ] */
+#define LF_KEY_GRAVE_ACCENT       ::LoFox::Key::GraveAccent		/* ` */
+#define LF_KEY_WORLD_1            ::LoFox::Key::World1			/* non-US #1 */
+#define LF_KEY_WORLD_2            ::LoFox::Key::World2			/* non-US #2 */
 
 /* Function keys */
-#define BG_KEY_ESCAPE             ::LoFox::Key::Escape		
-#define BG_KEY_ENTER              ::LoFox::Key::Enter		
-#define BG_KEY_TAB                ::LoFox::Key::Tab			
-#define BG_KEY_BACKSPACE          ::LoFox::Key::Backspace	
-#define BG_KEY_INSERT             ::LoFox::Key::Insert		
-#define BG_KEY_DELETE             ::LoFox::Key::Delete		
-#define BG_KEY_RIGHT              ::LoFox::Key::Right		
-#define BG_KEY_LEFT               ::LoFox::Key::Left		
-#define BG_KEY_DOWN               ::LoFox::Key::Down		
-#define BG_KEY_UP                 ::LoFox::Key::Up			
-#define BG_KEY_PAGE_UP            ::LoFox::Key::PageUp		
-#define BG_KEY_PAGE_DOWN          ::LoFox::Key::PageDown	
-#define BG_KEY_HOME               ::LoFox::Key::Home		
-#define BG_KEY_END                ::LoFox::Key::End			
-#define BG_KEY_CAPS_LOCK          ::LoFox::Key::CapsLock	
-#define BG_KEY_SCROLL_LOCK        ::LoFox::Key::ScrollLock	
-#define BG_KEY_NUM_LOCK           ::LoFox::Key::NumLock		
-#define BG_KEY_PRINT_SCREEN       ::LoFox::Key::PrintScreen	
-#define BG_KEY_PAUSE              ::LoFox::Key::Pause		
-#define BG_KEY_F1                 ::LoFox::Key::F1			
-#define BG_KEY_F2                 ::LoFox::Key::F2			
-#define BG_KEY_F3                 ::LoFox::Key::F3			
-#define BG_KEY_F4                 ::LoFox::Key::F4			
-#define BG_KEY_F5                 ::LoFox::Key::F5			
-#define BG_KEY_F6                 ::LoFox::Key::F6			
-#define BG_KEY_F7                 ::LoFox::Key::F7			
-#define BG_KEY_F8                 ::LoFox::Key::F8			
-#define BG_KEY_F9                 ::LoFox::Key::F9			
-#define BG_KEY_F10                ::LoFox::Key::F10			
-#define BG_KEY_F11                ::LoFox::Key::F11			
-#define BG_KEY_F12                ::LoFox::Key::F12			
-#define BG_KEY_F13                ::LoFox::Key::F13			
-#define BG_KEY_F14                ::LoFox::Key::F14			
-#define BG_KEY_F15                ::LoFox::Key::F15			
-#define BG_KEY_F16                ::LoFox::Key::F16			
-#define BG_KEY_F17                ::LoFox::Key::F17			
-#define BG_KEY_F18                ::LoFox::Key::F18			
-#define BG_KEY_F19                ::LoFox::Key::F19			
-#define BG_KEY_F20                ::LoFox::Key::F20			
-#define BG_KEY_F21                ::LoFox::Key::F21			
-#define BG_KEY_F22                ::LoFox::Key::F22			
-#define BG_KEY_F23                ::LoFox::Key::F23			
-#define BG_KEY_F24                ::LoFox::Key::F24			
-#define BG_KEY_F25                ::LoFox::Key::F25			
-#define BG_KEY_KP_0               ::LoFox::Key::KP0			
-#define BG_KEY_KP_1               ::LoFox::Key::KP1			
-#define BG_KEY_KP_2               ::LoFox::Key::KP2			
-#define BG_KEY_KP_3               ::LoFox::Key::KP3			
-#define BG_KEY_KP_4               ::LoFox::Key::KP4			
-#define BG_KEY_KP_5               ::LoFox::Key::KP5			
-#define BG_KEY_KP_6               ::LoFox::Key::KP6			
-#define BG_KEY_KP_7               ::LoFox::Key::KP7			
-#define BG_KEY_KP_8               ::LoFox::Key::KP8			
-#define BG_KEY_KP_9               ::LoFox::Key::KP9			
-#define BG_KEY_KP_DECIMAL         ::LoFox::Key::KPDecimal	
-#define BG_KEY_KP_DIVIDE          ::LoFox::Key::KPDivide	
-#define BG_KEY_KP_MULTIPLY        ::LoFox::Key::KPMultiply	
-#define BG_KEY_KP_SUBTRACT        ::LoFox::Key::KPSubtract	
-#define BG_KEY_KP_ADD             ::LoFox::Key::KPAdd		
-#define BG_KEY_KP_ENTER           ::LoFox::Key::KPEnter		
-#define BG_KEY_KP_EQUAL           ::LoFox::Key::KPEqual		
-#define BG_KEY_LEFT_SHIFT         ::LoFox::Key::LeftShift	
-#define BG_KEY_LEFT_CONTROL       ::LoFox::Key::LeftControl	
-#define BG_KEY_LEFT_ALT           ::LoFox::Key::LeftAlt		
-#define BG_KEY_LEFT_SUPER         ::LoFox::Key::LeftSuper	
-#define BG_KEY_RIGHT_SHIFT        ::LoFox::Key::RightShift	
-#define BG_KEY_RIGHT_CONTROL      ::LoFox::Key::RightControl
-#define BG_KEY_RIGHT_ALT          ::LoFox::Key::RightAlt	
-#define BG_KEY_RIGHT_SUPER        ::LoFox::Key::RightSuper	
-#define BG_KEY_MENU               ::LoFox::Key::Menu	
+#define LF_KEY_ESCAPE             ::LoFox::Key::Escape		
+#define LF_KEY_ENTER              ::LoFox::Key::Enter		
+#define LF_KEY_TAB                ::LoFox::Key::Tab			
+#define LF_KEY_BACKSPACE          ::LoFox::Key::Backspace	
+#define LF_KEY_INSERT             ::LoFox::Key::Insert		
+#define LF_KEY_DELETE             ::LoFox::Key::Delete		
+#define LF_KEY_RIGHT              ::LoFox::Key::Right		
+#define LF_KEY_LEFT               ::LoFox::Key::Left		
+#define LF_KEY_DOWN               ::LoFox::Key::Down		
+#define LF_KEY_UP                 ::LoFox::Key::Up			
+#define LF_KEY_PAGE_UP            ::LoFox::Key::PageUp		
+#define LF_KEY_PAGE_DOWN          ::LoFox::Key::PageDown	
+#define LF_KEY_HOME               ::LoFox::Key::Home		
+#define LF_KEY_END                ::LoFox::Key::End			
+#define LF_KEY_CAPS_LOCK          ::LoFox::Key::CapsLock	
+#define LF_KEY_SCROLL_LOCK        ::LoFox::Key::ScrollLock	
+#define LF_KEY_NUM_LOCK           ::LoFox::Key::NumLock		
+#define LF_KEY_PRINT_SCREEN       ::LoFox::Key::PrintScreen	
+#define LF_KEY_PAUSE              ::LoFox::Key::Pause		
+#define LF_KEY_F1                 ::LoFox::Key::F1			
+#define LF_KEY_F2                 ::LoFox::Key::F2			
+#define LF_KEY_F3                 ::LoFox::Key::F3			
+#define LF_KEY_F4                 ::LoFox::Key::F4			
+#define LF_KEY_F5                 ::LoFox::Key::F5			
+#define LF_KEY_F6                 ::LoFox::Key::F6			
+#define LF_KEY_F7                 ::LoFox::Key::F7			
+#define LF_KEY_F8                 ::LoFox::Key::F8			
+#define LF_KEY_F9                 ::LoFox::Key::F9			
+#define LF_KEY_F10                ::LoFox::Key::F10			
+#define LF_KEY_F11                ::LoFox::Key::F11			
+#define LF_KEY_F12                ::LoFox::Key::F12			
+#define LF_KEY_F13                ::LoFox::Key::F13			
+#define LF_KEY_F14                ::LoFox::Key::F14			
+#define LF_KEY_F15                ::LoFox::Key::F15			
+#define LF_KEY_F16                ::LoFox::Key::F16			
+#define LF_KEY_F17                ::LoFox::Key::F17			
+#define LF_KEY_F18                ::LoFox::Key::F18			
+#define LF_KEY_F19                ::LoFox::Key::F19			
+#define LF_KEY_F20                ::LoFox::Key::F20			
+#define LF_KEY_F21                ::LoFox::Key::F21			
+#define LF_KEY_F22                ::LoFox::Key::F22			
+#define LF_KEY_F23                ::LoFox::Key::F23			
+#define LF_KEY_F24                ::LoFox::Key::F24			
+#define LF_KEY_F25                ::LoFox::Key::F25			
+#define LF_KEY_KP_0               ::LoFox::Key::KP0			
+#define LF_KEY_KP_1               ::LoFox::Key::KP1			
+#define LF_KEY_KP_2               ::LoFox::Key::KP2			
+#define LF_KEY_KP_3               ::LoFox::Key::KP3			
+#define LF_KEY_KP_4               ::LoFox::Key::KP4			
+#define LF_KEY_KP_5               ::LoFox::Key::KP5			
+#define LF_KEY_KP_6               ::LoFox::Key::KP6			
+#define LF_KEY_KP_7               ::LoFox::Key::KP7			
+#define LF_KEY_KP_8               ::LoFox::Key::KP8			
+#define LF_KEY_KP_9               ::LoFox::Key::KP9			
+#define LF_KEY_KP_DECIMAL         ::LoFox::Key::KPDecimal	
+#define LF_KEY_KP_DIVIDE          ::LoFox::Key::KPDivide	
+#define LF_KEY_KP_MULTIPLY        ::LoFox::Key::KPMultiply	
+#define LF_KEY_KP_SUBTRACT        ::LoFox::Key::KPSubtract	
+#define LF_KEY_KP_ADD             ::LoFox::Key::KPAdd		
+#define LF_KEY_KP_ENTER           ::LoFox::Key::KPEnter		
+#define LF_KEY_KP_EQUAL           ::LoFox::Key::KPEqual		
+#define LF_KEY_LEFT_SHIFT         ::LoFox::Key::LeftShift	
+#define LF_KEY_LEFT_CONTROL       ::LoFox::Key::LeftControl	
+#define LF_KEY_LEFT_ALT           ::LoFox::Key::LeftAlt		
+#define LF_KEY_LEFT_SUPER         ::LoFox::Key::LeftSuper	
+#define LF_KEY_RIGHT_SHIFT        ::LoFox::Key::RightShift	
+#define LF_KEY_RIGHT_CONTROL      ::LoFox::Key::RightControl
+#define LF_KEY_RIGHT_ALT          ::LoFox::Key::RightAlt	
+#define LF_KEY_RIGHT_SUPER        ::LoFox::Key::RightSuper	
+#define LF_KEY_MENU               ::LoFox::Key::Menu	
 
-#define BG_KEY_UNKNOWN            ::LoFox::Key::Unknown	
+#define LF_KEY_UNKNOWN            ::LoFox::Key::Unknown	
