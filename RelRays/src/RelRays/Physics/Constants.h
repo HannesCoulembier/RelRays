4f200fec29e1a3eec5b82afbd794f8631603cde5
HannesCoulembier <hannes.coulembier@gmail.com>
2024-01-16 16:10:51 +0100

    Added pi and e to Constants namespace

--- RelRays/src/RelRays/Physics/Constants.h
+++ RelRays/src/RelRays/Physics/Constants.h
@@ -5,14 +5,16 @@
 namespace Constants {
 
 	// Speed of light
-	inline float c = 299792458 * Units::m / Units::s;
-
+	inline float c	= 299792458 * Units::m / Units::s;
 	// Planck constant
-	inline float h = 6.62607015 * 0.0001f * Prefix::q * Units::J * Units::s;
-
+	inline float h	= 6.62607015 * 0.0001f * Prefix::q * Units::J * Units::s;
 	// Boltzmann constant
-	inline float k = 1.380649 * 0.01f * Prefix::z * Units::J / Units::K;
-
+	inline float k	= 1.380649 * 0.01f * Prefix::z * Units::J / Units::K;
 	// Avogadro constant
-	inline float NA = 6.02214076 * 100.0f * Prefix::Z / Units::mol;
+	inline float NA	= 6.02214076 * 100.0f * Prefix::Z / Units::mol;
+
+	// Pi
+	inline float pi	= 3.141592653589793238462643383f;
+	// e
+	inline float e	= 2.7182818284590452353602874f;
 }
